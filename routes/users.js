85ba0fdfa1d0f0c19b1d552b0fbd31e934cd2dc0
HugoLMTY <47945757+HugoLMTY@users.noreply.github.com>
2021-02-23 16:25:33 +0100

    fix bcrypt

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const Product = require('../models/product')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-nodejs')
 const nodemailer = require('nodemailer')
 
 var mailVerif = 'hugo.lm2707@gmail.com'
